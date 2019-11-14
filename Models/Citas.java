8922bb5ca35b8fa4721d9ee9ad59c15dd77ebf42
CesarArellano <raywayday@gmail.com>
2019-11-14 10:38:34 -0600

    Modelos Avance 2.1

--- Models/Citas.java
+++ Models/Citas.java
@@ -1,27 +1,32 @@
 public class Citas{
     private String Fecha, Hora, Descripcion;
 
-    public Citas(String Fecha, String Hora, String Descripcion)
-    {
+    public Citas(String Fecha, String Hora, String Descripcion){
         this.SetFecha(Fecha);
         this.SetHora(Hora);
         this.SetDescripcion(Descripcion);
     }
+
     public String SetFecha(String Fecha){
         this.Fecha = Fecha;
     }
+
     public String GetFecha(){
         return Fecha;
     }
+
     public String SetHora(String Hora){
         this.Hora = Hora;
     }
+
     public String GetHora(){
         return Hora;
     }
+
     public String SetDescripcion(String Descripcion){
         this.Descripcion = Descripcion;
     }
+    
     public String GetDescripcion(){
         return Descripcion;
     }
